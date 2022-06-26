abf168cf65658e981788cffcbfc29851ec52d078
Luigi Improta <luigi.improta1998@gmail.com>
2022-06-26 13:27:03 +0200

    initial commit of the repo

---
+++ ExplorEAV/ExplorEAV/ExplorEAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorEAVApp.swift
+//  ExplorEAV
+//
+//  Created by Luigi Improta on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ExplorEAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileView()
+        }
+    }
+}
